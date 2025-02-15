d410ac9b094a554be669e227c15d531443dfcd3f
imaim <imaim@aty0607>
2025-02-15 16:48:29 +0900

    First.javaの修正

--- src/exercise/First.java
+++ src/exercise/First.java
@@ -3,10 +3,7 @@ package exercise;
 public class First {
 
 	public static void main(String[] args) {
-
-		System.out.println("eclipseをはじめよう");
-		System.out.println("お試し");
-		System.out.println("追加");
+System.out.println("別ファイル");
 
 	}
 
