140bb6c93dc9ea1bd7bf3f0792f0ba1d0599a3c2
imaim <imaim@aty0607>
2025-02-15 17:39:29 +0900

    second.javaから削除

--- src/exercise/Second.java
+++ src/exercise/Second.java
@@ -5,9 +5,6 @@ public class Second {
 	public static void main(String[] args) {
 
 		System.out.println("eclipseをはじめよう");
-		System.out.println("お試し");
-		System.out.println("追加");
-		System.out.println("追加2");
 
 	}
 
