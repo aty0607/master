f68b60f8fe8e98b8e54c7809e9fad3e592d8f97b
imaim <imaim@aty0607>
2025-02-15 17:16:20 +0900

    Second.javaの追加

---
+++ src/exercise/Second.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class Second {
+
+	public static void main(String[] args) {
+
+		System.out.println("eclipseをはじめよう");
+		System.out.println("お試し");
+		System.out.println("追加");
+		System.out.println("追加2");
+
+	}
+
+}
