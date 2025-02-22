2777235d138d09f25c26afad1f73c43b98348ae7
imaim <imaim@aty0607>
2025-02-22 22:23:24 +0900

    案件Cに伴う変更

--- src/exercise/First.java
+++ src/exercise/First.java
@@ -3,8 +3,31 @@ package exercise;
 public class First {
 
 	public static void main(String[] args) {
-System.out.println("別ファイル");
-System.out.println("案件B");
+
+		System.out.println("計算をします");
+		int a ;
+		int b ;
+		int c ;
+		a=20;
+		b=20;
+		c=30;
+		if(10==a) {
+			System.out.println(a+b);
+		}else {
+			int x;
+			x=a+b+c;
+			if(x!=0) {
+				System.out.println("終了");
+			}else {
+				System.out.println(x);
+				
+			}
+
+
+		}
+		
+
+		
 	}
 
 }
